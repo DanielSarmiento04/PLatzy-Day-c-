63602e4762813d44ce394cfbf1d901b92692b8c3
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2022-03-20 20:58:36 -0500

    🥑

--- Etapa1/Program.cs
+++ Etapa1/Program.cs
@@ -2,8 +2,8 @@
 
 
 var escuela = new Escuela(
-    nombre:"PLatzi",año:2002,
-    Tipo: TiposEscuela.Primaria, Pais :"Vancouver"
+    nombre: "PLatzi", año: 2002,
+    Tipo: TiposEscuela.Primaria, Pais: "Vancouver"
 );
 
 escuela.TipoEscuela = TiposEscuela.PreEscolar;
@@ -14,34 +14,63 @@ Console.WriteLine(escuela.ToString());
 
 var arregloCurso = new Curso[3];
 
-arregloCurso[0] = new Curso(){
+arregloCurso[0] = new Curso()
+{
     Nombre = "101"
 };
-var curso2 = new Curso(){
+var curso2 = new Curso()
+{
     Nombre = "202"
 };
 arregloCurso[1] = curso2;
 
-arregloCurso[2] = new Curso(){
+arregloCurso[2] = new Curso()
+{
     Nombre = "301"
 };
 System.Console.WriteLine("================");
 
-ImprimirCursos(arregloCurso);
+ImprimirCursosForEach(arregloCurso);
+// ImprimirCursosWhile(arregloCurso);
 
-void ImprimirCursos(Curso[] arregloCurso)
+void ImprimirCursosWhile(Curso[] arregloCurso)
 {
     int Cont = 0;
-    while ( Cont < arregloCurso.Count())
+    while (Cont < arregloCurso.Count())
     {
         Console.WriteLine($"{arregloCurso[Cont].Nombre}, {arregloCurso[Cont].UniqueId}");
         Cont++;
-    }    
+    }
 }
 
-foreach (var curso in arregloCurso)
+void ImprimirCursosDoWhile(Curso[] arregloCurso)
 {
-    Console.WriteLine($"{curso.Nombre}-{curso.UniqueId}");
-    Console.WriteLine("put Enter for continue");
-    Console.ReadLine();
+    int Cont = 0;
+    do
+    {
+        Console.WriteLine($"{arregloCurso[Cont].Nombre}, {arregloCurso[Cont].UniqueId}");
+        Cont++;
+
+    } while (Cont < arregloCurso.Count());
+}
+
+void ImprimirCursosFor(Curso[] arregloCurso)
+{
+    for (int i = 0; i < arregloCurso.Count(); i++)
+    {
+        Console.WriteLine($"{arregloCurso[i].Nombre}-{arregloCurso[i].UniqueId}");
+        Console.WriteLine("put Enter for continue");
+        Console.ReadLine();
+    }
+}
+
+
+void ImprimirCursosForEach(Curso[] arregloCurso)
+{
+    foreach (var curso in arregloCurso)
+    {
+        Console.WriteLine($"{curso.Nombre}-{curso.UniqueId}");
+        Console.WriteLine("put Enter for continue");
+        Console.ReadLine();
+    }
 }
